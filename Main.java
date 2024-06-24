292f5d666424f2dbf1db60801fa7ddd55e603594
tjhv10 <achiyabennatan@gmail.com>
2024-06-24 12:25:58 +0300

    final

--- Main.java
+++ Main.java
@@ -32,9 +32,9 @@ public class Main {
     }
 
     public static void printMatrix(double[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
+        for (double[] matrix1 : matrix) {
             for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j] + " ");
+                System.out.print(matrix1[j] + " ");
             }
             //System.out.println();
         }
@@ -43,7 +43,8 @@ public class Main {
         System.out.println();
         List<Grid> grids = Grid.readFile();
         double alpha = 0.1;
-        double epsilon = 0.1, sum= 0;
+        double epsilon = 0.1;
+        // double sum= 0;
         // int puzzle = 1;
         // for (Grid grid : grids) {
         //     System.out.println(puzzle++ + ":");
